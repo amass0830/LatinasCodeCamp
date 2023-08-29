0835ea33c828d443d4ad18d25ac4e5eb9b817117
Andrea Mass <amass0830@gmail.com>
2023-08-29 01:38:41 -0600

    Updates on final proyect

--- final_project_bootcamp/my-stock-bank/src/components/Login.jsx
+++ final_project_bootcamp/my-stock-bank/src/components/Login.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function Login({ realPin, setLoginStatus, fullname, setFullname, username, setUsername }) {
+function Login({ setLoginStatus, fullname, setFullname, username, setUsername }) {
   const [pin, setPin] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
+  const Dapin = 'Dapin';
+  const correctPassword = '4553'; 
+  
   const handleLogin = () => {
-    if (pin === realPin && password === 'password') { // Compare pin with realPin and password with 'password'
+    if (pin != Dapin && password != correctPassword) {
       setLoginStatus(true);
     } else {
       setErrorMessage('Credenciales incorrectas. Inténtalo de nuevo.');
@@ -16,14 +19,14 @@ function Login({ realPin, setLoginStatus, fullname, setFullname, username, setUs
 
   return (
     <div>
-      <h2>Iniciar Sesión :3 </h2>
+      <h2>Iniciar Sesión</h2>
       <div>
         <input
           type="text"
           placeholder="Nombre completo"
           value={fullname}
           onChange={(e) => setFullname(e.target.value)}
-          className="rounded-input" 
+          className="rounded-input"
         />
       </div>
       <div>
@@ -32,28 +35,30 @@ function Login({ realPin, setLoginStatus, fullname, setFullname, username, setUs
           placeholder="Nombre de usuario"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
-          className="rounded-input" 
+          className="rounded-input"
         />
       </div>
       <div>
         <input
           type="password"
           placeholder="PIN"
-          value={pin}
+          value={Dapin}
           onChange={(e) => setPin(e.target.value)}
-          className="rounded-input" 
+          className="rounded-input"
         />
       </div>
       <div>
         <input
           type="password"
           placeholder="Contraseña"
-          value={password}
+          value={4453}
           onChange={(e) => setPassword(e.target.value)}
-          className="rounded-input" 
+          className="rounded-input"
         />
       </div>
-      <button onClick={handleLogin}>Iniciar Sesión</button>
+      <button onClick={handleLogin} className="rounded-button">
+        Iniciar Sesión
+      </button>
       {errorMessage && <p className="error-message">{errorMessage}</p>}
     </div>
   );
